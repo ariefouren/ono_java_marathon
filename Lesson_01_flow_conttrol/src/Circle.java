b91f5491df4658be2eba995cc6a76ec1942b0098
ariefouren <aporan@ono.ac.il>
2023-01-19 23:00:11 +0200

    Create Circle.java

---
+++ Lesson_01_flow_conttrol/src/Circle.java
@@ -0,0 +1,15 @@
+/*
+ * Circle.java
+ */
+public class Circle
+{
+   public static void main(String[] args)
+   {
+      double radius = 100;
+      
+      double area = Math.PI * radius * radius;
+       
+      System.out.printf("radius = %.2f\n", radius);
+      System.out.printf("area   = %.2f\n", area);
+   }
+}
