9f9f2b4679485bee98cad4242515bf2730a54bea
ariefouren <aporan@ono.ac.il>
2023-01-19 23:05:29 +0200

    ZZ

--- Lesson_01_flow_conttrol/src/Circle.java
+++ Lesson_01_flow_conttrol/src/Circle.java
@@ -5,7 +5,7 @@ public class Circle
 {
    public static void main(String[] args)
    {
-      double radius = 100;
+      double radius = 100.0;
       
       double area = Math.PI * radius * radius;
        
