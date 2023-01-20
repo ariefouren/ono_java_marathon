8675173fa05f422d3667337d3182a507f5089d54
ariefouren <aporan@ono.ac.il>
2023-01-20 10:42:23 +0200

    Create FV.java

---
+++ Lesson_01_flow_conttrol/src/FV.java
@@ -0,0 +1,22 @@
+
+public class FV {
+
+	public static void main(String[] args) {
+		
+		double pv = 1000.0;
+		double rate = 0.05;
+		int nper = 25;
+		
+		double fv = pv * Math.pow((1+rate), nper); // fv = pv(1+r)^n
+		
+		// user output
+		System.out.printf("pv   = %.2f\n", pv);
+		System.out.printf("rate = %.2f\n", rate);
+		System.out.printf("nper = %d\n", nper);
+		
+		
+		
+
+	}
+
+}
