40b673f707544f08a2f4cacdaf59d86849321e02
ariefouren <aporan@ono.ac.il>
2023-01-23 21:02:30 +0200

    ZZ

--- Lesson_02_loops_and_arrays/src/ArrayExample.java
+++ Lesson_02_loops_and_arrays/src/ArrayExample.java
@@ -4,10 +4,11 @@ public class ArrayExample {
 	public static void main(String[] args) {
 		
 		// definition of array
-		double[] array = new double[10];
+		double[] array = new double[100];
+		
 		
 		// print the array
-		for(int i = 0; i < 10; i++)
+		for(int i = 0; i < 100; i++)
 		{
 			System.out.printf("array[%d] = %.2f \n", i, array[i]);
 		}
