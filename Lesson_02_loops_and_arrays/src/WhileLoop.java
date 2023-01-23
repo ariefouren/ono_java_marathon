495e9fa583dca9ab8308923b44103c092bd5e222
ariefouren <aporan@ono.ac.il>
2023-01-23 20:26:39 +0200

    XX
    zz

---
+++ Lesson_02_loops_and_arrays/src/WhileLoop.java
@@ -0,0 +1,24 @@
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		
+		double pv = 1000.0;
+		double rate = 0.05;
+		int nper = 0;
+		
+		double fv = pv; 
+		
+		while(fv < 2000)
+		{
+			fv = fv * (1 + rate); 
+			nper ++;
+			System.out.printf("nper = %d, fv = %.2f\n", nper, fv);
+		}
+		
+		
+		
+
+	}
+
+}
