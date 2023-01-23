301b6ff41bc8486b085bcdae515afb5af8408277
ariefouren <aporan@ono.ac.il>
2023-01-23 21:42:06 +0200

    Update Order.java

--- Lesson_02_loops_and_arrays/src/Order.java
+++ Lesson_02_loops_and_arrays/src/Order.java
@@ -25,24 +25,29 @@ public class Order {
 		
 		// computation
 		/*
-		 * Number of items:      5 V
-Total price:        258.64
-Min price:           10.99
-Max price:           10.99
-Average price:       51.73
+		 * Number of items:      5 	V
+Total price:        258.64			V
+Min price:           10.99			V
+Max price:           100.99			V
+Average price:       51.73  <--
 
 		 */
 		double totalPrice = 0;
+		double minPrice = prices[0];
+		double maxPrice = prices[0];
 		for(int i = 0; i < numItems; i++)
 		{
 			totalPrice += prices[i];
+			if(prices[i] < minPrice) {minPrice = prices[i];}
+			if(prices[i] > maxPrice) {maxPrice = prices[i];}
 		}
+		double averagePrice = totalPrice/numItems;
 		
 		System.out.printf("Number of items :   %d\n", numItems);
 		System.out.printf("Total price     :   %.2f\n", totalPrice);
-		
-		
-
+		System.out.printf("Max price       :   %.2f\n", maxPrice);
+		System.out.printf("Min price       :   %.2f\n", minPrice);
+		System.out.printf("Average price   :   %.2f\n", averagePrice);
 	}
 
 }
