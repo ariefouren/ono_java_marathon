50ffa88f27de12eb7f4e5b61b424519db1efaf85
ariefouren <aporan@ono.ac.il>
2023-01-30 20:40:15 +0200

    SS

---
+++ Lesson_04_Classes_and_Objects/src/BankBranch.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class BankBranch {
+	
+	private String branchName;
+	private ArrayList<BankAccount> accounts;
+	
+	public BankBranch(String branchName) {
+		this.branchName = branchName;
+		this.accounts = new ArrayList<BankAccount>();
+	}
+	
+	
+	
+	
+	
+	
+
+}
